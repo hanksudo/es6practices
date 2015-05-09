98910d2380d3e07ab3d490db873ca755e8d67352
Hank Wang <drapho@gmail.com>
2015-05-09 20:04:31 +0800

    init environment

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var watch = require("gulp-watch");
+
+gulp.task("default", function() {
+    return gulp.src("src/*.js")
+    .pipe(watch("src/*.js"))
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+});
